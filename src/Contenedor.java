14f0395c974d874859cdf7ab26c9eac4002c52a9
AgustinRebechi <rebechi.agustin@gmail.com>
2025-05-24 19:49:19 -0300

    1.0

--- src/Contenedor.java
+++ src/Contenedor.java
@@ -1,4 +1,71 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
-public class Contenedor {
+public class Contenedor<T> {
+
+    private T[] elementos;
+    private int size = 0;
+
+    public Contenedor(int capacidad) {
+        elementos = (T[]) new Object[capacidad];
+    }
+
+    public void agregar(T elemento) {
+        if (size >= elementos.length) {
+            elementos = Arrays.copyOf(elementos, elementos.length + 1); // O duplicar capacidad
+        }
+        elementos[size++] = elemento;
+    }
+
+    public T obtener(int indice) {
+        return elementos[indice];
+    }
+
+    public void quitar(int indice) {
+        if (indice< 0 || indice >= size) {
+            throw new IndexOutOfBoundsException("Índice inválido");
+        }
+
+        @SuppressWarnings("unchecked")
+        T[] nuevo = (T[]) new Object[size - 1];
+
+        for (int i = 0, j = 0; i < size; i++) {
+            if (i != indice) {
+                nuevo[j++] = elementos[i];
+            }
+        }
+        elementos = nuevo;
+        size--;
+    }
+
+    public void verificar(int indice) {
+        if (elementos[indice] == null) {
+            System.out.println("indice vacio");
+        } else {
+            System.out.println("indice ocupado");
+            System.out.println("el elemento es: " + elementos[indice]);
+        }
+    }
+
+    public void visualizar() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(elementos[i]);
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Contenedor<?> otro = (Contenedor<?>) obj;
+        return Arrays.equals(this.elementos, otro.elementos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(elementos);
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        Contenedor<Object> caja = new Contenedor<>(2);
+
+        caja.agregar(1);
+        caja.agregar("hola");
+        caja.visualizar();
+        caja.quitar(1);
+        caja.verificar(0);
+        caja.visualizar();
+
+        //equals
+
+        Contenedor<Object> caja2 = new Contenedor<>(1);
+        caja2.agregar(1);
+        caja2.visualizar();
+        System.out.println(caja.equals(caja2));
+
+
+    }
 }
